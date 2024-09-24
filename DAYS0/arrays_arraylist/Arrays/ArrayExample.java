16ab7889a2714fb1db06371c731dba400336af9a
pappukrs <digitalpappu101@gmail.com>
2024-09-24 09:23:17 +0530

    Arrays

---
+++ DAYS0/arrays_arraylist/Arrays/ArrayExample.java
@@ -0,0 +1,22 @@
+
+import java.util.Arrays;
+
+
+class ArrayExample {
+
+    public static void main(String[] args){
+
+        // Create an array of integers with a fixed size
+
+        int[] numbers = new int[5];
+
+        numbers[0]=1;
+        numbers[1]=2;
+        numbers[2]=3;
+        numbers[3]=4;
+
+        System.out.println("numbers"+" "+Arrays.toString(numbers));
+
+
+    }
+}
