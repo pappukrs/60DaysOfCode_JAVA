1284187b097e060591b183ae7b38a97b70caac1e
pappukrs <digitalpappu101@gmail.com>
2024-09-25 19:32:03 +0530

    hashmap

---
+++ DAY1/HashMap/HashmapExample.java
@@ -0,0 +1,10 @@
+
+import java.util.HashMap;
+class HashmapExample{
+    public static void main(String args[]){
+            HashMap<String,Integer> hash =     new HashMap<>();
+              hash.put("Alka", 1);
+              hash.put("Sharma",43);
+            System.out.println("hash"+hash);
+    }
+}
